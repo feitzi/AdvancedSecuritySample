1a879f104a02e7d1c556f7c39b40d0ca506a4835
Mathias.Feitzinger <mathias.feitzinger@world-direct.at>
2022-07-15 23:22:47 +0200

    insecure redirect with minimalApi2

--- AdvancedSecuritySample/Controllers/WeatherForecastController.cs
+++ AdvancedSecuritySample/Controllers/WeatherForecastController.cs
@@ -32,6 +32,6 @@ public class WeatherForecastController : ControllerBase {
             return Redirect(newURl);
 
         }
-        return Redirect("BAD" + newURl);
+        return Redirect("BAD");
     }
 }
